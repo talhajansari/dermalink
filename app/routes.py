867e463a5cd36244b1f8b39b9677f801d91a7b42
Kevin Michael Mangan <kmm2256@columbia.edu>
2014-04-13 18:01:46 -0400

    committing before pull

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from flaskext.uploads import UploadSet, configure_uploads, IMAGES
 from forms import LoginForm, SignupForm, CreateIssueForm, DermSignupForm
-from models import User, Image, Issue, Dermatologist, Patient, Doctor
+from models import User, Image, Issue, Patient, Doctor
 
 reserved_usernames = 'home signup login logout post'
 
