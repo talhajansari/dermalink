9cf838552c0b96deb3c90530153fb2366400d60a
Talha Ansari <talhajansari@hotmail.com>
2014-08-11 02:24:59 -0400

    Automatic environ var setting

--- run-dev.py
+++ run-dev.py
@@ -1,4 +1,7 @@
 #!flask/bin/python
+import os
+os.environ['SERVER_ENV'] = 'DEV'
+
 from app import app
 app.run (debug=True)
 #app.run(host='0.0.0.0')
--- run-stg.py
+++ run-stg.py
@@ -1,3 +1,5 @@
 #!flask/bin/python
+import os
+os.environ('SERVER_ENV') = 'STG'
 from app import app
 app.run(host='0.0.0.0')
